1ee920b9a46a19cbafa4f441a4036d1b613906b3
user <github email address>
2020-11-10 08:30:47 -0500

    commit tree 7e5acb74494a9fa3250077bd1743575bab5496d4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
+    
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
 
   def count_sentences
+    if self.split.count == 3
+      self.split.count
+    else 
+      self.split( /, */, 4 ).count 
+    end
+  end 
 
-  end
+    
+
 end
+
+# def parse
+#   @emails.split(/, | /).uniq
+  
+
+# end 
+
+
+# complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+
